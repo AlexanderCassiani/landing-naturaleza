993ca299e3c589c1d326ec905f5c718edee6b7b9
AlexanderCassiani <alexandercassiani74@gmail.com>
2026-09-01 21:32:35 -0500

    Agrega el componente Hero y su hoja de estilos

---
+++ src/components/hero/Hero.jsx
@@ -0,0 +1,28 @@
+import "./hero.css";
+
+const Hero = () => {
+  return (
+    <div className="hero">
+      <video
+        width="100%"
+        height="100%"
+        src="/video-naturaleza.mp4"
+        autoPlay
+        muted
+        loop
+      ></video>
+
+      <div className="overlay"></div>
+
+      <div className="hero-contenedor">
+        <h1 className="hero-titulo">Conecta con la naturaleza</h1>
+        <p className="hero-descripcion">
+          Pequeños cambios en tu día a día hacen una gran diferencia por el
+          planeta.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
