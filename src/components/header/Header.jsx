3cd4491155b8ba43c9c734e8a950e4ca4303fbf0
AlexanderCassiani <alexandercassiani74@gmail.com>
2026-09-02 16:18:01 -0500

    Agrega menú móvil con estilos y funcionalidad para la navegación

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,12 +1,15 @@
 import "./header.css";
+import { useState } from "react";
 
 const Header = () => {
+  const [mostrarMenu, setMostrarMenu] = useState(false)
+
   return (
     <header>
-      <div>
+      <div className="contenedor-logo">
         <img src="logo.png" alt="Logo" />
       </div>
-      <div>
+      <div className="ocultar">
         <nav>
           <ul>
             <li>
@@ -17,6 +20,28 @@ const Header = () => {
             <li>
               <a href="#cuidemos-seccion" className="abajo">
                 <span>Cuidemos</span>
+              </a>
+            </li>
+          </ul>
+        </nav>
+      </div>
+
+      {/*menu mobile */}
+      <div className="contenedor-menu-hamburguesa" onClick={() => setMostrarMenu(!mostrarMenu)}>
+        <span className={mostrarMenu ? "rotar-palito-1" : null}>-</span>
+        <span className={mostrarMenu ? "ocultar-palito-2" : null}>-</span>
+        <span className={mostrarMenu ? "rotar-palito-3" : null}>-</span>
+
+        <nav className={mostrarMenu ? "nav-mobile mostrar-nav-mobile" : "nav-mobile"}>
+          <ul>
+            <li>
+              <a href="#hero" className="arriba">
+                Inicio
+              </a>
+            </li>
+            <li>
+              <a href="#cuidemos-seccion" className="abajo">
+                Cuidemos
               </a>
             </li>
           </ul>
